edad47ffad99dc931689ec4bf5be519c39d54bce
Josphat karanja <josphatranja243@gmail.com>
2023-01-20 05:34:21 +0300

    Middleware and Mongoose

---
+++ middleware/error-handler.js
@@ -0,0 +1,7 @@
+const errorHandlerMiddleware = async (err, req, res, next) => {
+    console.log(err);
+    return res.status(500).json({
+        msg: 'Something went Wrong, Please try again'
+    })
+}
+module.exports = errorHandlerMiddleware;
