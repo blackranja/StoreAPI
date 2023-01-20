dfe52b9325283a9bd67fdb05d85cb11e59bbbca2
Josphat karanja <josphatranja243@gmail.com>
2023-01-20 06:11:13 +0300

    Middleware and Mongoose

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
 const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 
 //middleware
+
 app.use(express.json());
+
 
 
 //routes
@@ -19,6 +22,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
+
+app.use('/api/v1/products', productsRouter);
 
 
 //Products route
