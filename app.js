dc10e0b068bff9acbfffcb341ab2bf1477806f1d
Josphat karanja <josphatranja243@gmail.com>
2023-01-20 09:21:51 +0300

    Mongoose Schemas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
-//async errors
 
+//async errors__________________________________________________________
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
@@ -11,13 +12,13 @@ const errorMiddleware = require('./middleware/error-handler');
 const connectDB = require('./db/connect');
 const productsRouter = require('./routes/products');
 
-//middleware
+//middleware_____________________________________________________________
 
 app.use(express.json());
 
 
 
-//routes
+//routes___________________________________________________________________________
 
 app.get('/', (req, res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
@@ -26,7 +27,7 @@ app.get('/', (req, res) => {
 app.use('/api/v1/products', productsRouter);
 
 
-//Products route
+//Products route______________________________________________________________
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
@@ -36,9 +37,11 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
-        //connectDB
+
+        //connectDB__________________________________________
+
         await connectDB(process.env.MONGO_URI);
-        app.listen(port, console.log(`server is listening to port ${port}`));
+        app.listen(port, console.log(`server is listening to port ${port} .....`));
 
     } catch (error) {
         console.log(error);
--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
 const getAllProductsStatic = async (req, res) => {
+    throw new Error('Testing Async Errors');
     res.status(200).json({
         msg: 'Products testing route'
     })
