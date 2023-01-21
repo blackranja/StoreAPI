3cae1c89a7f5968146709e44fa83545288b9ff30
Josphat karanja <josphatranja243@gmail.com>
2023-01-21 13:43:04 +0300

    Mongoose Queries

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,36 @@
+const Product = require('../models/product');
+
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('Testing Async Errors');
-    res.status(200).json({
-        msg: 'Products testing route'
-    })
+    const products = await Product.find({});
+    res.status(200).json({ products })
 
 }
 
+
 const getAllProducts = async (req, res) => {
+    const { featured, company, name } = req.query;
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+
+    }
+
+    if (company) {
+        queryObject.company = company
+    }
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' };
+    }
+
+    console.log(queryObject);
+
+    const products = await Product.find(queryObject);
     res.status(200).json({
-        msg: 'Products route'
+        products,
+        nbHits: products.length
     })
 }
 
